b3c9a6fa94fb129a9ad08f6a8077cf9254ea5e9f
zakarish <90837069+zakarish@users.noreply.github.com>
2022-11-07 17:53:13 +0100

    Create login.php

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>username = Airbus350; password = 787</p>
+    <form method="post" action="data.php" enctype="multipart/form-data">
+        <lable for="email">usrename</lable>
+        <br>
+        <input type="text" name="user">
+        <br>
+        <lable for="password">lösenord</lable>
+        <br>
+        <input type="text" name="password">
+        <br>
+        <input type="submit" value="shush" name="submit">
+    </form>
+</body>
+</html>
